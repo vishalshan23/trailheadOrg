e4d2fdbccc7a6fb1fbd1d8682feb30bd36ad8443
Vishal Shanbhag <DK8934@falck.com>
2019-08-20 16:54:39 +0200

    Connect Components with Events module changes

--- force-app/main/default/aura/expenseForm/expenseFormHelper.js
+++ force-app/main/default/aura/expenseForm/expenseFormHelper.js
@@ -1,5 +1,8 @@
 ({
-    helperMethod : function() {
-
+    createExpense: function(component, newExpense) {
+        var createEvent = component.getEvent("createExpense");
+        createEvent.setParams({ "expense": newExpense });
+        createEvent.fire();
     }
+
 })
--- force-app/main/default/aura/expenses/expensesController.js
+++ force-app/main/default/aura/expenses/expensesController.js
@@ -15,23 +15,14 @@
         // Send action off to be executed
         $A.enqueueAction(action);
     },
-    clickCreate: function(component, event, helper) {
-        var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        // If we pass error checking, do some real work
-        if(validExpense){
-            // Create the new expense
-            var newExpense = component.get("v.newExpense");
-            console.log("Create expense: " + JSON.stringify(newExpense));
-            helper.createExpense(component, newExpense);
-        }
-    },
     handleUpdateExpense: function(component, event, helper) {
         var updatedExp = event.getParam("expense");
         helper.updateExpense(component, updatedExp);
+    },
+    handleCreateExpense: function(component, event, helper) {
+        var newExpense = event.getParam("expense");
+        helper.createExpense(component, newExpense);
     }
+
 
 })
